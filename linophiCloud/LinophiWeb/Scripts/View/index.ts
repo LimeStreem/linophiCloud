00384c8cdfa54f4c79d87f4b278ba936bb410b63
LimeStreem <LimeStreem@gmail.com>
2014-08-15 03:36:26 +0900

    川瀬の作った読むページのマージ

---
+++ linophiCloud/LinophiWeb/Scripts/View/index.ts
@@ -0,0 +1,12 @@
+﻿var verticalStreacher: HeightStretcher;
+var horizontalStreacher: WidthStretcher;
+$(() => {
+    verticalStreacher = new HeightStretcher($("article"), $("#container"));
+    verticalStreacher.addSubElement($("header"));
+    verticalStreacher.addSubElement($("footer"));
+    verticalStreacher.updateTargetProperty();
+    horizontalStreacher = new WidthStretcher($("#right"), $("article"));
+    horizontalStreacher.addSubElement($("#left"));
+    horizontalStreacher.ExtraMargin = 2;
+    horizontalStreacher.updateTargetProperty();
+});
