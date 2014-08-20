b1d0c68b1fddeab1ff4a2207f604fb9ee9dcefd5
yuya sato <omikositteiiyone@live.jp>
2014-08-20 17:10:13 +0900

    現状 幾らかのページ追加

--- linophiCloud/LinophiWeb/Controllers/HomeController.cs
+++ linophiCloud/LinophiWeb/Controllers/HomeController.cs
@@ -30,7 +30,37 @@ namespace LinophiWeb.Controllers
             return View();
         }
 
+        public ActionResult Follower()
+        {
+            return View();
+        }
+
+        public ActionResult Favorites()
+        {
+            return View();
+        }
+
+        public ActionResult Ranking()
+        {
+            return View();
+        }
+
+        public ActionResult Notification()
+        {
+            return View();
+        }
+
         public ActionResult NewArticle()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
+        public ActionResult MyProduction()
         {
             return View();
         }
