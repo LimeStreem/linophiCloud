874f5bcaee6ddab85cc9ff5149f321acdbbb81bb
LimeStreem <LimeStreem@gmail.com>
2014-08-21 14:23:51 +0900

    Fix vertical editer

---
+++ linophiCloud/LinophiWeb/Scripts/Home/AfterLogin.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $(".title-tooltiper").attr("title", '<div class="tip-container"><h1 class="tip-book-title">無職転生</h1><h2 class="tip-book-auther">理不尽な孫の手:著<h2><p></div>');
+    $(".title-tooltiper").tooltipster({
+        contentAsHTML: true,
+        position: "bottom"
+    });
+});
+//# sourceMappingURL=AfterLogin.js.map
---
+++ linophiCloud/LinophiWeb/Scripts/Home/AfterLogin.ts
@@ -0,0 +1,7 @@
+﻿$(() => {
+    $(".title-tooltiper").attr("title", '<div class="tip-container"><h1 class="tip-book-title">無職転生</h1><h2 class="tip-book-auther">理不尽な孫の手:著<h2><p></div>');
+    $(".title-tooltiper").tooltipster({
+        contentAsHTML:true,
+        position:"bottom"
+    });
+});
